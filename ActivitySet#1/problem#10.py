4a5421aa04134ecd552718e685f59b402958d7fe
kancharla devaharshini <94678326+KDevaHarshini@users.noreply.github.com>
2022-07-05 15:33:21 +0000

    problem#10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,11 @@
-# Dictionaries
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    l=line.split()
+    for i in l:
+        if i not in lst:
+            lst.append(i)
+lst.sort()
+print(lst)
 
-filename = "dataset/mbox-short.txt"
